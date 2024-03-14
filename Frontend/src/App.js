ac2fbbe2e3db43a72018b8b0f8fbf42893403977
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2024-03-14 23:40:36 +0800

    added browser router

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import Payment from './Components/Pages/Legal/Payment';
 import Shipping from './Components/Pages/Legal/Shipping';
 import Returns from './Components/Pages/Legal/Returns';
 import Contact from './Components/Pages/Contact';
-
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
-
 import './App.css';
-
 import './fonts/RedHatDisplay-VariableFont_wght.ttf';
 
 function App() {
   return (
     <div className="font">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/payment" element={<Payment/>}/>
-        <Route path="/shipping" element={<Shipping/>}/>
-        <Route path="/returns" element={<Returns/>}/>
-      </Routes>
-      <Footer/>
+      <Router basename="/First-Floor"> {/* Add basename */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/shipping" element={<Shipping />} />
+          <Route path="/returns" element={<Returns />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }

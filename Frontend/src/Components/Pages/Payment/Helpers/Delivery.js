314fd143b56c2db3f372e465397af1f2c12b899b
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2024-04-02 15:39:58 +0800

    changed delivery form

--- Frontend/src/Components/Pages/Payment/Helpers/Delivery.js
+++ Frontend/src/Components/Pages/Payment/Helpers/Delivery.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-
 import { Text, Box, FormControl, Input, FormLabel, Select, Button } from '@chakra-ui/react';
 
-const Delivery =() => {
+const Delivery = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [address, setAddress] = useState('');
@@ -12,7 +11,6 @@ const Delivery =() => {
   const [phoneNumber, setPhoneNumber] = useState('');
 
   const handleSubmit = async () => {
-    // Construct the user details object
     const userDetails = {
       firstName,
       lastName,
@@ -23,55 +21,56 @@ const Delivery =() => {
       phone: phoneNumber,
     };
 
-    // TODO: Send `userDetails` to your backend to handle Stripe customer creation/updation
     console.log(userDetails);
+    // TODO: Send `userDetails` to your backend to handle Stripe customer creation/updation
   };
 
-  return(
+  return (
     <Box mb={"50px"}>
       <Text fontWeight={"bold"} fontSize={"2xl"} mb={"20px"}>Delivery</Text>
 
       <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
-        <FormControl mb={"20px"}>
-          <Input placeholder={"First Name"} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        <FormControl mb={"20px"} isRequired>
+          <Input placeholder={"First Name"} value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
         </FormControl>
 
-        <FormControl mb={"20px"}>
-          <Input placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        <FormControl mb={"20px"} isRequired>
+          <Input placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
         </FormControl>
 
-        <FormControl mb={"20px"}>
-          <Input placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
+        <FormControl mb={"20px"} isRequired>
+          <Input placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} required />
         </FormControl>
 
-        <FormControl mb={"20px"}>
-          <Input placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} />
+        <FormControl mb={"20px"} isRequired>
+          <Input placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} required />
         </FormControl>
 
-        <FormControl mb={"20px"}>
-          <Select placeholder="State/Territory" value={state} onChange={(e) => setState(e.target.value)}>
-            <option>Western Australia</option>
-            <option>Australian Capital Territory</option>
-            <option>New South Wales</option>
-            <option>Northern Territory</option>
-            <option>Queensland</option>
-            <option>South Australia</option>
-            <option>Tasmania</option>
-            <option>Victoria</option>
-          </Select>
+        <FormControl mb={"20px"} isRequired>
+  <Select value={state} onChange={(e) => setState(e.target.value)} required>
+    <option value="" disabled selected>Select State/Territory</option>
+    <option value="Western Australia">Western Australia</option>
+    <option value="Australian Capital Territory">Australian Capital Territory</option>
+    <option value="New South Wales">New South Wales</option>
+    <option value="Northern Territory">Northern Territory</option>
+    <option value="Queensland">Queensland</option>
+    <option value="South Australia">South Australia</option>
+    <option value="Tasmania">Tasmania</option>
+    <option value="Victoria">Victoria</option>
+  </Select>
+</FormControl>
+
+        <FormControl mb={"20px"} isRequired>
+          <Input placeholder="Post Code" value={postCode} onChange={(e) => setPostCode(e.target.value)} required />
         </FormControl>
 
-        <FormControl mb={"20px"}>
-          <Input placeholder="Post Code" value={postCode} onChange={(e) => setPostCode(e.target.value)} />
+        <FormControl mb={"20px"} isRequired>
+          <Input placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required />
         </FormControl>
 
-        <FormControl mb={"20px"}>
-          <Input placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-        </FormControl>
-
-        <Button type="submit" colorScheme="blue">Submit</Button>
       </form>
     </Box>
   );
 };
+
 export default Delivery;
